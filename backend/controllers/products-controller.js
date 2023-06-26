ead33deb67ddfeff13fcbe848038427efc4e256d
ellrussbest <ellrussbest@gmail.com>
2023-06-26 20:04:50 +0200

    some changes

--- backend/controllers/products-controller.js
+++ backend/controllers/products-controller.js
@@ -8,9 +8,8 @@
 
 // product should populate categories, transactio
 
-const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
 const getCoordsForAddress = require("../utils/location");
-const { Product, Category, Transaction } = require("../models");
+const { Product, Category, Transaction, HttpError } = require("../models");
 const { default: mongoose } = require("mongoose");
 const fs = require("fs");
